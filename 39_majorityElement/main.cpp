8903505a7c5676026798ee19a599581ed3a4b040
bjtangye <bjtangye@163.com>
2020-07-02 16:11:41 +0800

    39

---
+++ 39_majorityElement/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+/*
+数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。
+你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+
+示例 1:
+
+输入: [1, 2, 3, 2, 2, 2, 5, 4, 2]
+输出: 2
+
+
+限制：
+
+1 <= 数组长度 <= 50000
+
+ */
+
+int majorityElement(vector<int>& nums) {
+   unordered_map<int, int> m;
+   int halfsize = nums.size() / 2;
+
+   for(int i : nums)
+   {
+       if(m.find(i) != m.end())
+       {
+           m[i]++;
+       }
+       else
+       {
+           m[i] = 1;
+       }
+   }
+
+   for(auto i : m)
+   {
+       if(i.second > halfsize)
+       {
+           return i.first;
+       }
+   }
+
+   return -1;
+}
+
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
